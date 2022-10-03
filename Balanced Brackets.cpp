a0c134414ff993abfd104a7e1320a5a6fa87c2e2
Krish Patel <patel.12102325@lpu.in>
2022-10-03 22:13:55 +0530

    Create Balanced Brackets.cpp
    By this logic, we say a sequence of brackets is balanced if the following conditions are met:
    
    It contains no unmatched brackets.
    The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.

---
+++ Balanced Brackets.cpp
@@ -0,0 +1,141 @@
+/*
+A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+It contains no unmatched brackets.
+The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, return YES. Otherwise, return NO.
+
+Function Description
+
+Complete the function isBalanced in the editor below.
+
+isBalanced has the following parameter(s):
+
+string s: a string of brackets
+Returns
+
+string: either YES or NO
+Input Format
+
+The first line contains a single integer , the number of strings.
+Each of the next  lines contains a single string , a sequence of brackets.
+
+Constraints
+
+, where  is the length of the sequence.
+All chracters in the sequences ∈ { {, }, (, ), [, ] }.
+Output Format
+
+For each string, return YES or NO.
+
+Sample Input
+
+STDIN           Function
+-----           --------
+3               n = 3
+{[()]}          first s = '{[()]}'
+{[(])}          second s = '{[(])}'
+{{[[(())]]}}    third s ='{{[[(())]]}}'
+Sample Output
+
+YES
+NO
+YES
+Explanation
+
+The string {[()]} meets both criteria for being a balanced string.
+The string {[(])} is not balanced because the brackets enclosed by the matched pair { and } are not balanced: [(]).
+The string {{[[(())]]}} meets both criteria for being a balanced string.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+string isBalanced(string s) {
+   stack<char> check;
+    string result="NO";
+    int n=s.length();
+    for(int i=0;i<n;i++)
+    {
+        if(check.empty())
+        {
+            check.push(s[i]);
+        }
+        else if((s[i] == '}' && check.top() == '{') || (s[i] == ']' && check.top() == '[') || (s[i] == ')' && check.top() == '('))
+        {
+            check.pop();
+        }
+        else
+        {
+            check.push(s[i]);
+        }
+    }
+    if(check.empty())
+    {
+        result="YES";
+        return result;
+    }
+    return result;
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
